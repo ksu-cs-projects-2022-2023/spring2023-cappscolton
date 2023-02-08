e19a23d908a32261ea1ca74af3ce9a2bc6c43e8f
Colton Capps <cjcapps@ksu.edu>
2023-02-08 02:23:06 -0600

    fix test route href

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1 class="font-bold">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="font-bold">Hello world</h1>
+<p>Test <a href="/test">this</a> route.</p>
