9b19770fafa12706f60576480e258cc7a945ee89
Colton Capps <cjcapps@ksu.edu>
2023-05-12 21:48:17 -0500

    general launch endpoint for LTI cartridge to home page

--- src/routes/api/tp/launch/+server.ts
+++ src/routes/api/tp/launch/+server.ts
@@ -1,9 +1,8 @@
 import type { RequestHandler } from "./$types";
-import { redirect } from '@sveltejs/kit';
+import { redirect } from "@sveltejs/kit";
 
 // Receive launch request from canvas - redirect to platform page
-export const POST = (async ({ params, request, url, platform }) => {
+export const POST = (async () => {
   // TODO: validate oauth signature from canvas (low prio)
-  console.log(await params);
-  throw redirect(302, '/platform');
+  throw redirect(302, "/platform");
 }) satisfies RequestHandler;
