38f4adc11c444ecd02f4aa3782edc67f5077851d
Colton Capps <cjcapps@ksu.edu>
2023-05-12 21:40:11 -0500

    add prisma config to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { createRequire } from "module";
+
+// TODO: This is a hack to fix a prisma issue that occurs in Prod (serverless edge environment)
+// https://github.com/prisma/prisma/issues/12504
+const prismaPlugin = () => {
+  const require = createRequire(import.meta.url);
+  const pathName = require
+    .resolve("@prisma/client")
+    .replace("@prisma/client/index.js", "");
+
+  return {
+    name: "prisma-vite-plugin",
+    config: () => ({
+      resolve: {
+        alias: {
+          ".prisma/client/edge": `${pathName}.prisma/client/edge`,
+        },
+      },
+    }),
+  };
+};
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  ssr: {
-    noExternal:
-      process.env.NODE_ENV === "production"
-        ? ["@carbon/charts", "carbon-components"]
-        : [],
-  },
+  plugins: [sveltekit(), prismaPlugin()],
 });
