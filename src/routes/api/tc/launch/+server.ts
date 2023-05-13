80b1a8014e749c05918a612962e77ac2b316f486
Colton Capps <cjcapps@ksu.edu>
2023-05-12 21:54:11 -0500

    deleting outdated stuff

--- src/routes/api/tc/launch/+server.ts
+++
@@ -1,5 +0,0 @@
-import type { RequestHandler } from "./$types";
-
-export const GET = (async ({ request, url, platform }) => {
-    return new Response('{"result":"success"}');
-}) satisfies RequestHandler;
--- src/routes/platform/components/LtiParam.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  /** @type {import('./$types').PageData} */
-  export let value = "";
-  export let name = "";
-</script>
-
-<input type="hidden" {name} {value} />
