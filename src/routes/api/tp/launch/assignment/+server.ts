4a9d67d15c959645b57dc7f668ffad0a3485b443
Colton Capps <cjcapps@ksu.edu>
2023-05-12 21:48:42 -0500

    endpoint to receive LTI assignment launch

--- src/routes/api/tp/launch/assignment/+server.ts
+++ src/routes/api/tp/launch/assignment/+server.ts
@@ -1,21 +1,38 @@
 import type { RequestHandler } from "./$types";
-import { redirect } from '@sveltejs/kit';
+import { redirect } from "@sveltejs/kit";
 
 // Receive launch request from canvas - redirect to platform page
 export const POST = (async ({ params, request, url, platform }) => {
-    // TODO: validate oauth signature from canvas (low prio)
-    var formEncodedParams = await request.text()
-    var paramArray = formEncodedParams.split('&');
+  // TODO: validate oauth signature from canvas (low prio)
+  var formData = await request.formData();
 
-    const resultMap = new Map();
+  if (formData.get("user_id") == null) {
+    console.log(
+      "no user_id sent from canvas: you are probaby logged in as canvas admin"
+    );
+  }
 
-    paramArray.forEach(str => {
-        const [key, value] = str.split("=");
-        resultMap.set(key, value);
-    });
-
-    console.log(resultMap.get("lis_outcome_service_url"));
-    console.log(resultMap.get("lis_result_sourcedid"));
-
-    throw redirect(302, `/platform/assignment?lis_outcome_service_url%3D${resultMap.get("lis_outcome_service_url")}&lis_result_sourcedid%3D${resultMap.get("lis_result_sourcedid")}`);
+  throw redirect(
+    302,
+    // params that I want to forward to the platform,
+    // so that I can store them and use them to identify facts about the LMS / course / user
+    // prettier-ignore
+    `/platform/assignment/${
+      encodeURIComponent(formData.get("lis_outcome_service_url")!.toString()
+    )}/${
+      encodeURIComponent(formData.get("lis_result_sourcedid")!.toString()
+    )}/${
+      encodeURIComponent(formData.get("roles")!.toString())
+    }/${
+      encodeURIComponent(formData.get("user_id")!.toString())
+    }/${
+      encodeURIComponent(formData.get("context_id")!.toString())
+    }/${
+      encodeURIComponent(formData.get("lis_person_name_family")!.toString())
+    }/${
+      encodeURIComponent(formData.get("lis_person_name_given")!.toString())
+    }/${
+      encodeURIComponent(formData.get("lis_person_contact_email_primary")!.toString())
+    }`
+  );
 }) satisfies RequestHandler;
